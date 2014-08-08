7e00ca0ce210e78f89804402e2f0d2c8a73d4513
Dan Vera <dan@yovu.co>
2014-08-08 18:38:13 -0400

    table style

--- js/main.js
+++ js/main.js
@@ -23,5 +23,35 @@ $(document).ready(function() {
   $('.thumbnail').nivoLightbox();
 
 
+  // socket connector to blockchain
+
+  //var conn = new WebSocket('ws://ws.blockchain.info/inv');
+  //
+  //conn.onopen = function () {
+  //  console.log('open');
+  //  conn.send('{"op":"unconfirmed_sub"}');
+  //}
+
+  //conn.onclose = function () {
+  //  console.log('close');
+  //}
+  //
+  //conn.onerror = function (error) {
+  //  console.log('websocket error: ' + error);
+  //}
+  //
+  //conn.onmessage = function (message) {
+  //  var response = JSON.parse(message.data);
+
+  //  var amount = 0;
+
+  //  for ( var i = 0; i < response.x.out.length; i++ ) {
+  //    amount += response.x.out[i].value;
+  //  }
+
+  //  $(".transactions tbody").prepend('<tr><td class="t-hash"><a href="https://blockchain.info/tx/'+response.x.hash+'" target="_blank">'+response.x.hash+'</a></td><td class="t-time"><span data-livestamp="'+response.x.time+'"></span></td><td class="t-amount"><span class="label label-success">'+amount+' BTC</span></td></tr>');
+  //}
+
+
 });
 
