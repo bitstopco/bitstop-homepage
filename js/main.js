30c17fe369db7597c0133aa8c6a56c73075dca3b
Dan Vera <dan@yovu.co>
2014-08-08 18:43:50 -0400

    blockchain socket api for transactions

--- js/main.js
+++ js/main.js
@@ -25,32 +25,37 @@ $(document).ready(function() {
 
   // socket connector to blockchain
 
-  //var conn = new WebSocket('ws://ws.blockchain.info/inv');
-  //
-  //conn.onopen = function () {
-  //  console.log('open');
-  //  conn.send('{"op":"unconfirmed_sub"}');
-  //}
+  var conn = new WebSocket('ws://ws.blockchain.info/inv');
+  
+  conn.onopen = function () {
+    console.log('open');
+    conn.send('{"op":"unconfirmed_sub"}');
+  }
 
-  //conn.onclose = function () {
-  //  console.log('close');
-  //}
-  //
-  //conn.onerror = function (error) {
-  //  console.log('websocket error: ' + error);
-  //}
-  //
-  //conn.onmessage = function (message) {
-  //  var response = JSON.parse(message.data);
+  conn.onclose = function () {
+    console.log('close');
+  }
+  
+  conn.onerror = function (error) {
+    console.log('websocket error: ' + error);
+  }
+  
+  conn.onmessage = function (message) {
+    var response = JSON.parse(message.data);
 
-  //  var amount = 0;
+    var amount = 0;
 
-  //  for ( var i = 0; i < response.x.out.length; i++ ) {
-  //    amount += response.x.out[i].value;
-  //  }
+    for ( var i = 0; i < response.x.out.length; i++ ) {
+      amount += response.x.out[i].value;
+    }
+    
+    response.amount = amount / 100000000;
+    amount = response.amount;
 
-  //  $(".transactions tbody").prepend('<tr><td class="t-hash"><a href="https://blockchain.info/tx/'+response.x.hash+'" target="_blank">'+response.x.hash+'</a></td><td class="t-time"><span data-livestamp="'+response.x.time+'"></span></td><td class="t-amount"><span class="label label-success">'+amount+' BTC</span></td></tr>');
-  //}
+    console.log(amount);
+
+    $(".transactions tbody").prepend('<tr><td class="t-hash"><a href="https://blockchain.info/tx/'+response.x.hash+'" target="_blank">'+response.x.hash+'</a></td><td class="t-time"><span data-livestamp="'+response.x.time+'"></span></td><td class="t-amount"><span class="label label-success">'+amount+' BTC</span></td></tr>');
+  }
 
 
 });
