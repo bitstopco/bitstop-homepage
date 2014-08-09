76ea7a0972c0953c31bda18d1e489ff6247f18a5
Dan Vera <dan@yovu.co>
2014-08-09 15:59:42 -0400

    init

--- index.php
+++ index.php
@@ -1,144 +1,21 @@
 <?php
-  include('lib/functions.php');
+  require 'vendor/autoload.php';
+
+  Dotenv::load(__DIR__); # Load env variables
+
+  $app = new \Slim\Slim(array(
+    'debug' => true
+  ));
+
+  $app->setName('bitstop');
+
+  //Routes
+  $app->get('/', 'home');
+
+  # lets go
+  $app->run();
+
+  function home() {
+    echo "You shall not pass";
+  }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?php echo $config['title']; ?></title>
-  <link href="css/style.css" rel="stylesheet">
-
-  <meta property="og:title" content="<?php echo $config['title']; ?>"/>
-  <meta property="og:url" content="<?php echo $config['url']; ?>"/>
-  <meta property="og:type" content="website"/>
-  <meta property="og:description" content="Bitcoin Services Coming To South Florida - Fall 2014" />
-  <meta property="og:image" content="<?php echo $config['url']; ?>/img/favicon.png" />
-
-  <link rel="shortcut icon" type="image/png" href="img/favicon.png">
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-  <![endif]-->
-</head>
-<body>
-  <div class="doc-loader"></div>
-  <!--/////////// HEADER SECTION //////////-->
-  <section id="intro">
-    <header>
-      <nav class="navbar navbar-default" role="navigation">
-        <div class="container">
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="container">
-
-            <a class="navbar-brand" href="index.php">
-              <img src="img/logo.png">
-            </a>
-
-            <div id="main-nav" class="navbar-collapse">          
-              <ul class="social-links">
-                <li class="current-value">1 BTC = <?php echo BTCprice('1'); ?></li>
-                <a href="#"><li class="fa fa-twitter"></li></a><!-- Twitter Link -->
-                <a href="#"><li class="fa fa-facebook"></li></a><!-- Facebook Link -->
-                <a href="#"><li class="fa fa-instagram"></li></a><!-- Instagram Link -->
-              </ul>
-            </div><!-- /navbar-collapse -->
-          </div><!-- /container -->
-        </div>
-      </nav>
-    </header>
-    
-    <div class="container">
-      <div class="intro-well wow animated bounceIn">
-        <div class="col-md-12">
-          <h1>Bitcoin Services Coming To South Florida</h1>
-          <h2>Fall 2014</h2>
-        </div>
-      </div>
-    </div><!-- /container -->
-  </section>
-
-  <!--////////// LATEST TRANSACTION SECTION ////////// -->
-  <section id="features">
-    <div class="container wow fadeIn animated">
-      <h1>Latest Transactions</h1>
-      <h2>See transactions as they happen live.</h2>
-    </div><!-- /container -->
-    
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12">
-
-          <div class="panel" style="border-bottom: 0px; border-left: 0px; border-right: 0px;">
-            <div class="panel-body transaction-container">
-              <table class="table table-striped transactions">
-                <tbody>
-                  
-                </tbody>
-              </table>
-            </div>
-          </div>
-
-        </div>
-      </div><!-- /row -->
-    </div><!-- /container -->
-  </section>
-
-  <!--////////// WHATS BITCOIN SECTION ////////// -->
-  <section id="features" class="what-is-bitcoin">
-    <div class="container wow fadeIn animated">
-      <h1>Bitcoin is an innovative payment network <br> and a new kind of money.</h1>
-    </div><!-- /container -->
-    
-    <div class="container">
-      <div class="row">
-        
-        <div class="what-is-it">
-          <iframe width="560" height="315" src="//www.youtube.com/embed/Gc2en3nHxA4?rel=0" frameborder="0" allowfullscreen></iframe>
-        </div>
-
-      </div><!-- /row -->
-    </div><!-- /container -->
-  </section>
-
-  <!--////////// NEWSLETTER SECTION ////////// -->
-  <section id="newsletter">
-
-    <div class="container wow animated fadeInDown">
-      <h3>Sign up today to be notified when we launch!</h3>
-
-      <h4 class="return-message" style="display: none;">It worked!</h4>
-
-      <h4 class="return-message-error" style="display: none;">It seems like theres a problem, try again.</h4>
-
-      <form class="form-inline newsletter-field">
-        <input type="email" id="email" name="email" placeholder="Enter your email..." class="input-lg">
-        <button type="submit" class="btn btn-primary">Subscribe</button>
-      </form>
-
-
-    </div>
-
-  </section>
-  
-  <!--////////// FOOTER SECTION ////////// -->
-  <footer>
-    <p>© Copyright 2014 BITSTOP</p>
-  </footer>
-
-  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-  <!-- Include all compiled plugins (below), or include individual files as needed -->
-  <script src="js/bootstrap.js"></script>
-  <script src="js/wow.min.js"></script>
-  <script src="js/nivo-lightbox.min.js"></script>
-  <script src="js/main.js"></script>
-  <script src="js/moment.js"></script>
-  <script src="js/livestamp.min.js"></script>
-</body>
-</html>
