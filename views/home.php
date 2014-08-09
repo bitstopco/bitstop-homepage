83561c6422b1069c03b82c2d07153dc96acb630a
Dan Vera <dan@yovu.co>
2014-08-09 16:58:11 -0400

    gaug.es

--- views/home.php
+++ views/home.php
@@ -140,5 +140,19 @@
   <script src="<?php get('url'); ?>/assets/js/moment.js"></script>
   <script src="<?php get('url'); ?>/assets/js/livestamp.min.js"></script>
 
+  <script type="text/javascript">
+    var _gauges = _gauges || [];
+    (function() {
+      var t   = document.createElement('script');
+      t.type  = 'text/javascript';
+      t.async = true;
+      t.id    = 'gauges-tracker';
+      t.setAttribute('data-site-id', '53e68b2f8bfdf7252100651c');
+      t.src = '//secure.gaug.es/track.js';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(t, s);
+    })();
+  </script>
+
 </body>
 </html>
